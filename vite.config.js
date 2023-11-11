abbe966169065f1c1255eed5ddca71f7840cb8a7
MissDaizy <dianaukrainsky@gmail.com>
2023-11-12 01:51:10 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-app/",
+  base: "/Diana-Ukrainsky-31-10-2023/",
   plugins: [svgr(), react()],
 });
