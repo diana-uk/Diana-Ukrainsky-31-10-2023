004cda0193ce481881956b0ad14ee4f18b83f3ab
MissDaizy <dianaukrainsky@gmail.com>
2023-11-12 14:04:54 +0200

    Deleted proxy from vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,4 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   base: "/Diana-Ukrainsky-31-10-2023/",
   plugins: [svgr(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://dataservice.accuweather.com/",
-        changeOrigin: true,
-        onProxyReq: (proxyReq) => {
-          // Manually remove the "/api" prefix from the outgoing request path
-          proxyReq.path = proxyReq.path.replace(/^\/api/, "");
-        },
-      },
-    },
-  },
 });
